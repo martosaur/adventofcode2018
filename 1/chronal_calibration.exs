8e1022c94504bf1cb1a573e8c86e15d1be43c5a2
martosaur <martosaur@gmail.com>
2018-12-01 22:27:53 +0100

    use proper data structures

--- 1/chronal_calibration.exs
+++ 1/chronal_calibration.exs
@@ -13,15 +13,15 @@ defmodule ChronalCalibration do
     |> sum_until_duplicate_accum()
   end
 
-  def sum_until_duplicate_accum(list), do: sum_until_duplicate_accum([0], list, list)
+  def sum_until_duplicate_accum(list), do: sum_until_duplicate_accum({0, MapSet.new([0])}, list, list)
   def sum_until_duplicate_accum(acc, initial_list, []), do: sum_until_duplicate_accum(acc, initial_list, initial_list)
-  def sum_until_duplicate_accum([h | _] = acc, initial_list, [next | rest]) do
-    new_sum = next + h
+  def sum_until_duplicate_accum({last_sum, history}, initial_list, [next | rest]) do
+    new_sum = next + last_sum
 
-    if new_sum in acc do
+    if MapSet.member?(history, new_sum) do
       new_sum
     else
-      sum_until_duplicate_accum([new_sum | acc], initial_list, rest)
+      sum_until_duplicate_accum({new_sum, MapSet.put(history, new_sum)}, initial_list, rest)
     end
   end
 end
